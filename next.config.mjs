88dd72731569c5f7fe929ee74a4223aa726d093b
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-03 04:44:24 +0530

    changed backend

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     // BACKEND_URI: "http://localhost:4000",
-    BACKEND_URI: "https://chiko-backend.onrender.com",
+    BACKEND_URI: "https://chiko-backend.vercel.app",
   },
   images: {
     remotePatterns: [
