b17457290482937f9b820e309fd56736608b01df
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-10-02 21:54:36 +0530

    layout fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster position="top-center" />
         <Navbar />
