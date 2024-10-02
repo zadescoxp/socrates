35fd365452f32724cb4eae3b4c8739dd81f7ed40
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-10-02 21:28:02 +0530

    footer init

---
+++ src/components/footer.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import {motion} from 'framer-motion'
+import Image from 'next/image'
+import { textVar } from '@/utils/variant'
+
+export default function Footer () {
+    return (
+        <div className="flex flex-col justify-center items-center leading-none mt-20 w-screen bg-[#C6C19E] py-20">
+          <motion.h1
+            variants={textVar}
+            initial="initial"
+            animate="animate"
+            className="relative font-bold text-[200px] flex items-center gap-4"
+          >
+            SOCRATES
+            <Image
+              src="/assets/vector1.svg"
+              alt="Vector"
+              height={150}
+              width={150}
+            />
+          </motion.h1>
+          <motion.p>©2024 All Rights Reserved</motion.p>
+        </div>
+    )
+}
