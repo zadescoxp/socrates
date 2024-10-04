8bcb0def92d46d36b0cad16d6aea2c6a19311cb3
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-10-04 21:30:05 +0530

    footer improved

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,27 +1,28 @@
-'use client'
+"use client";
 
-import {motion} from 'framer-motion'
-import Image from 'next/image'
-import { textVar } from '@/utils/variant'
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { textVar } from "@/utils/variant";
 
-export default function Footer () {
-    return (
-        <div className="flex flex-col justify-center items-center leading-none mt-20 w-screen bg-[#C6C19E] py-20">
-          <motion.h1
-            variants={textVar}
-            initial="initial"
-            animate="animate"
-            className="relative font-bold text-[200px] flex items-center gap-4"
-          >
-            SOCRATES
-            <Image
-              src="/assets/vector1.svg"
-              alt="Vector"
-              height={150}
-              width={150}
-            />
-          </motion.h1>
-          <motion.p>©2024 All Rights Reserved</motion.p>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className="flex flex-col justify-center items-center leading-none mt-20 w-screen bg-[#C6C19E] py-20">
+      <motion.h1
+        variants={textVar}
+        initial="initial"
+        animate="animate"
+        className="relative font-bold text-[200px] flex items-center gap-4 max-md:text-[120px] max-sm:text-[70px]"
+      >
+        SOCRATES
+        <Image
+          className="max-md:hidden"
+          src="/assets/vector1.svg"
+          alt="Vector"
+          height={150}
+          width={150}
+        />
+      </motion.h1>
+      <motion.p>©2024 All Rights Reserved</motion.p>
+    </div>
+  );
 }
